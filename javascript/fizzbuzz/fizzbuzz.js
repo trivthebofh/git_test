dac995411cb4e02f99ad37b83f4424c563776a70
Travis Mehrer <travis@canerduh.com>
2023-06-22 16:33:36 -0600

    Finished FizzBuzz

---
+++ javascript/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,20 @@
+ // Your JavaScript goes here!
+
+function fizzbuzz(num) {
+    for (let i = 1; i <= num; i++) {
+        if  (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+ let userNumber = parseInt(prompt("Enter a number to count to: "))
+ fizzbuzz(userNumber)
+
+ console.log(userNumber)
